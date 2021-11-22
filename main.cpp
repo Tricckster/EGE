647065ec658bf27358099e08524358ff13250076
Mad <1233@gmail.com>
2021-11-22 22:19:35 +0300

    print if %154==0

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void number_of_products_div()
 	+ 1.читать и запоминать числа
 	+ 2.вывести пары
 	+ 3.вместо пар, считать произведения
-	4.только пары, кратные 154
+	+ 4.только пары, кратные 154
 	5.только кол-во пар
 	*/
 	int n(0);
@@ -19,7 +19,8 @@ void number_of_products_div()
 	    cin >> numbers[i];
 	for (int j(0); j<n-1; j++)
 	    for (int i(j+1); i<n; i++)
-	        cout << numbers[j] * numbers[i] << endl;
+	        if ((numbers[i]*numbers[j])%154==0)
+	        cout << numbers[i]*numbers[j] << endl;
 }
 
 int main()
