03b3a1a04e4e00b8204775e41715f7c4e71bfe21
Mad <1233@gmail.com>
2021-11-22 22:12:19 +0300

    print pairs

--- main.cpp
+++ main.cpp
@@ -17,8 +17,9 @@ void number_of_products_div()
 	vector<int> numbers(n, 0);
 	for (int i(0); i<n; i++)
 	    cin >> numbers[i];
-	for (int i(0); i<n; i++)
-	    cout << numbers[i] << " ";
+	for (int j(0); j<n-1; j++)
+	    for (int i(j+1); i<n; i++)
+	        cout << numbers[j] << " " << numbers[i] << endl;
 }
 
 int main()
