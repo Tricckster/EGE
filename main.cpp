0b0e39cb13a465723c21ccc62b10a1ec18e5bd3c
Mad <1233@gmail.com>
2021-11-22 22:00:40 +0300

    create work plan

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void number_of_products_div()
+{
+	/**
+	1.читать и запоминать числа
+	2.вывести пары
+	3.вместо пар, считать произведения
+	4.только пары, кратные 154
+	5.только кол-во пар
+	*/
+	int n(0);
+	cin >> n;
+	vector<int> numbers(n, 0);
+	for (int i(0); i<n; i++)
+	    cin >> numbers[i];
+	for (int i(0); i<n; i++)
+	    cout << numbers[i] << " ";
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div();
     return 0;
 }
 
